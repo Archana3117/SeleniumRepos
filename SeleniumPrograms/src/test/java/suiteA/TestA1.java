976c402ce14026a7d7571d818dabc97945f10487
Archana3117 <archanamahajan1220@gmail.com>
2024-12-13 11:46:49 +0530

    added a b c variable into class TestA1

--- SeleniumPrograms/src/test/java/suiteA/TestA1.java
+++ SeleniumPrograms/src/test/java/suiteA/TestA1.java
@@ -5,7 +5,8 @@ import org.testng.annotations.Test;
 public class TestA1 {
   @Test
   public void TestA1() throws Exception {
-	  
+	 
+	  int a,b,c;
 	  System.out.println("TestA1 is starting");
 	  Thread.sleep(2000);
 	  System.out.println("TestA1 is ending");
