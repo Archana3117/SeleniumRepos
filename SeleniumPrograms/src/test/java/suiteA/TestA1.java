f0c78c076fa826225ac20aa5ff540232a8fe80d3
Archana3117 <archanamahajan1220@gmail.com>
2024-12-13 12:05:22 +0530

    added x,y,z variables into TestA in Bangluru branch

--- SeleniumPrograms/src/test/java/suiteA/TestA1.java
+++ SeleniumPrograms/src/test/java/suiteA/TestA1.java
@@ -6,6 +6,7 @@ public class TestA1 {
   @Test
   public void TestA1() throws Exception {
 	  
+	  int x,y,z;
 	  System.out.println("TestA1 is starting");
 	  Thread.sleep(2000);
 	  System.out.println("TestA1 is ending");
